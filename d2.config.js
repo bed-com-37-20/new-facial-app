30d22b7ae8c54eb76e588cee3f67713e3168ec4f
edsonhancale <110174899+edsonhancale@users.noreply.github.com>
2023-07-06 12:46:23 +0200

    feat(.eslintrc.json): add eslintrc dependencies and eslintrc file

--- d2.config.js
+++ d2.config.js
@@ -2,11 +2,7 @@ const config = {
     type: 'app',
 
     entryPoints: {
-<<<<<<< HEAD
-        app: './src/App.js',
-=======
         app: './src/App.tsx',
->>>>>>> 0fd2a09f27e6696d16c7a2ad03338a64d78225e8
     },
 }
 
