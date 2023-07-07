b9b1237c9f4a5202497e814ddbfbdb6faf1c43d9
edsonhancale <110174899+edsonhancale@users.noreply.github.com>
2023-07-07 11:48:02 +0200

    feat: added renderRow component

---
+++ src/components/table/components/index.tsx
@@ -0,0 +1,7 @@
+import RowCell from "./row/RowCell"
+import RowTable from "./row/RowTable"
+import HeadTable from "./head/HeadTable"
+import HeadCell from "./head/HeaderCell"
+import Pagination from "./pagination/Pagination"
+
+export { RowCell, RowTable, HeadTable, HeadCell, Pagination } 
---
+++ src/components/table/render/RenderHeader.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RenderHeader() {
+  return (
+    <div>RenderHeader</div>
+  )
+}
+
+export default RenderHeader
