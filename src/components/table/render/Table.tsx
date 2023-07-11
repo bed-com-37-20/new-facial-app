69d9cdc1bfa61bd069999d0986a835492355aa24
edsonhancale <110174899+edsonhancale@users.noreply.github.com>
2023-07-11 16:18:08 +0200

    fix: fixed bug when render table data EDSKELETON-34

--- src/components/table/render/Table.tsx
+++ src/components/table/render/Table.tsx
@@ -3,6 +3,7 @@ import { TableComponent } from '../components'
 import RenderHeader from './RenderHeader'
 import RenderRows from './RenderRows'
 import { makeStyles } from '@material-ui/core/styles';
+import { Paper } from '@material-ui/core';
 
 const usetStyles = makeStyles({
     tableContainer: {
@@ -13,29 +14,52 @@ const usetStyles = makeStyles({
 function Table() {
     const classes = usetStyles()
     return (
-        <div
-            // eslint-disable-next-line react/prop-types
-            className={classes.tableContainer}
-        >
-            <TableComponent>
-                <>
-                    <RenderHeader
-                        createSortHandler={() => { }}
-                        order='asc'
-                        orderBy='desc'
-                        rowsHeader={[]}
-                    />
-                    <RenderRows
-                        headerData={[{
-                            id: 'id',
-                            header: 'ID',
-                            optionSets: []
-                        }]}
-                        rowsData={[]}
-                    />
-                </>
-            </TableComponent>
-        </div>
+        <Paper>
+            <div
+                // eslint-disable-next-line react/prop-types
+                className={classes.tableContainer}
+            >
+                <TableComponent>
+                    <>
+                        <RenderHeader
+                            createSortHandler={() => { }}
+                            order='asc'
+                            orderBy='desc'
+                            rowsHeader={[
+                                {
+                                    id: 'id',
+                                    header: 'ID',
+                                    optionSets: []
+                                },
+                                {
+                                    id: 'id2',
+                                    header: 'ID2',
+                                    optionSets: []
+                                }
+                            ]}
+                        />
+                        <RenderRows
+                            headerData={[
+                                {
+                                    id: 'id',
+                                    header: 'ID',
+                                    optionSets: []
+                                },
+                                {
+                                    id: 'id2',
+                                    header: 'ID2',
+                                    optionSets: []
+                                }
+                            ]}
+                            rowsData={[
+                                { id: "111", id2: "2222" },
+                                { id: "222", id2: "2222" }
+                            ]}
+                        />
+                    </>
+                </TableComponent>
+            </div>
+        </Paper>
     )
 }
 
